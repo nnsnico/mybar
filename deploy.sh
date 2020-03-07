1d9aaef7f4793fae8a2e95d95fa29ef01cf3fa11
nnsnico <yuji.jampan.blue@gmail.com>
2020-03-07 19:30:53 +0900

    Fix link path

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-ln -snf $(pwd)/index.jsx /Users/nns/Library/Application\ Support/Übersicht/widgets/mybar.jsx
-ln -snf $(pwd)/font.css /Users/nns/Library/Application\ Support/Übersicht/widgets
+ln -snf $(pwd)/index.jsx $HOME/Library/Application\ Support/Übersicht/widgets/mybar.jsx
+ln -snf $(pwd)/font.css $HOME/Library/Application\ Support/Übersicht/widgets
