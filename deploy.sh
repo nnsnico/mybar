0febd6c07ab87be9cd7df64fc47a4bf247802b55
nnsnico <yuji.jampan.blue@gmail.com>
2023-05-07 13:52:21 +0900

    chore: refactor code

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-ln -snf $(pwd)/index.jsx $HOME/Library/Application\ Support/Übersicht/widgets/mybar.jsx
-ln -snf $(pwd)/font.css $HOME/Library/Application\ Support/Übersicht/widgets
+ln -snf "$(pwd)/index.jsx" "$HOME"/Library/Application\ Support/Übersicht/widgets/mybar.jsx
+ln -snf "$(pwd)/font.css" "$HOME"/Library/Application\ Support/Übersicht/widgets
